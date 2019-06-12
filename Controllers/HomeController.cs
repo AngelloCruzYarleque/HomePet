476a5427abcf94d92ca7e9673538231fd0fe7482
Kevin Humareda <alumno@ciis.usil.edu.pe>
2019-06-12 16:10:19 -0500

    Estableciendo

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,26 +18,28 @@ namespace HomePet.Controllers
             this._context = c;
             
         }
-        public IActionResult Index(string edad, string tamano, string tipoPelo)
+        public IActionResult Index(string edad, string tamano, string sexo)
         {            
             var mascotas = _context.Mascotas.ToList();
-            if(tipoPelo==null && edad==null && tamano==null){
+            if(sexo==null && edad==null && tamano==null){
                 mascotas = _context.Mascotas.OrderByDescending(x=>x.Id).ToList();
-            }else{
-                if(edad=="0" && tamano=="0" && tipoPelo!="0"){
-                    mascotas = _context.Mascotas.Where(x=>x.TipoPelo==tipoPelo).ToList();
-                }else if(edad=="0" && tamano!="0" && tipoPelo=="0"){
+            }else{     
+                if(sexo=="0" && edad=="0" && tamano=="0"){
+                    mascotas = _context.Mascotas.OrderByDescending(x=>x.Id).ToList();              
+                }else if(edad=="0" && tamano=="0" && sexo!="0"){
+                    mascotas = _context.Mascotas.Where(x=>x.Sexo==sexo).ToList();
+                }else if(edad=="0" && tamano!="0" && sexo=="0"){
                     mascotas = _context.Mascotas.Where(x=>x.Tamano==tamano).ToList();
-                }else if(edad!="0" && tamano=="0" && tipoPelo=="0"){
+                }else if(edad!="0" && tamano=="0" && sexo=="0"){
                     mascotas = _context.Mascotas.Where(x=>x.Edad==edad).ToList();
                 }else if(edad=="0"){
-                    mascotas = _context.Mascotas.Where(x=>x.Tamano==tamano && x.TipoPelo==tipoPelo).ToList();
+                    mascotas = _context.Mascotas.Where(x=>x.Tamano==tamano && x.Sexo==sexo).ToList();
                 }else if(tamano=="0"){
-                    mascotas = _context.Mascotas.Where(x=>x.Edad==edad && x.TipoPelo==tipoPelo).ToList();
-                }else if(tipoPelo=="0"){
+                    mascotas = _context.Mascotas.Where(x=>x.Edad==edad && x.Sexo==sexo).ToList();
+                }else if(sexo=="0"){
                     mascotas = _context.Mascotas.Where(x=>x.Tamano==tamano && x.Edad==edad ).ToList();
                 }else{
-                    mascotas = _context.Mascotas.Where(x=>x.Tamano==tamano && x.Edad==edad && x.TipoPelo==tipoPelo).ToList();
+                    mascotas = _context.Mascotas.Where(x=>x.Tamano==tamano && x.Edad==edad && x.Sexo==sexo).ToList();
                 }
             }
             ViewBag.m = mascotas;
